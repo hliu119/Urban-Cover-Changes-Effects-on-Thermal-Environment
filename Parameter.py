ce2a6915a74ea44346ef0e105e14c5e657cb6ed2
hliu119 <63568080+hliu119@users.noreply.github.com>
2020-11-03 12:10:28 -0500

    Add files via upload

---
+++ Parameter.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20 18:44:06 2019
+
+@author: HaoranLiu
+"""
+#input the number of the points 
+Point = { "Point_Number" : *              
+          }
+
+#csv store data of each point 
+CSV_path = {
+     Path" : r"***",
+        }
+
+#output the result
+Results_path = {
+    #"plot_Path" : "***", 
+    #"csv_Path" : "***", 
+        }
+
+
+    
